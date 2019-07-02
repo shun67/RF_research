4724c13a7b6088399df0ab75150d73edf30f7632
shun67 <52106552+shun67@users.noreply.github.com>
2019-07-02 23:22:50 +0900

    Update learn.py

--- learn.py
+++ learn.py
@@ -10,19 +10,17 @@ import sys
 
 
 print("loading start")
-train_X=pd.read_table('/home/hanbo/work/next_Deep/20181016_to_MBSJ2018/data/pssm/window15/train_data/train_train_w15_non_lbl2.fct',index_col=0)
+train_X=pd.read_table('*************',index_col=0)
 
-train_y=pd.read_table('/home/hanbo/work/next_Deep/20181016_to_MBSJ2018/data/pssm/window15/train_data/train_train_w15_non_lbl2.lbl',index_col=0)
+train_y=pd.read_table('*************',index_col=0)
 
-#pandas file->dataflame
-#JOIN dataflame
 #training data
-#JOIN dataflame
 print("train")
 clf = RandomForestClassifier(n_estimators=1000,bootstrap=True,n_jobs=-1,random_state=0)
 learn_time_s=time.time()
 clf = clf.fit(train_X, train_y)
 learn_time=time.time()-learn_time_s
 filename = 'RF_model_15.sav'
+#save model
 pickle.dump(clf, open(filename, 'wb'))
 print('learn time :%f[sec]'%learn_time)
